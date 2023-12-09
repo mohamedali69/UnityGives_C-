a8696c3cb10401608e1f7b7bf5ca8705a001fef4
mohamedali69 <dalichakhari6801@gmail.com>
2023-12-09 18:17:56 +0100

    add project folders

---
+++ CRUDWinFormsMVP/Views/MainView.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace CRUDWinFormsMVP.Views
+{
+    partial class MainView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnPets = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnPets);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(267, 695);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnPets
+            // 
+            this.btnPets.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPets.Location = new System.Drawing.Point(4, 66);
+            this.btnPets.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPets.Name = "btnPets";
+            this.btnPets.Size = new System.Drawing.Size(263, 46);
+            this.btnPets.TabIndex = 0;
+            this.btnPets.Text = "Events";
+            this.btnPets.UseVisualStyleBackColor = true;
+            this.btnPets.Click += new System.EventHandler(this.btnPets_Click);
+            // 
+            // MainView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1617, 695);
+            this.Controls.Add(this.panel1);
+            this.IsMdiContainer = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainView";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainView";
+            this.Load += new System.EventHandler(this.MainView_Load);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnPets;
+    }
+}
